e3cf53447ce390ebb3679b2be56123710234789f
feven mulugeta <fevenmulugeta@fevens-MacBook-Pro.local>
2018-11-13 20:06:38 -0600

    google button

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
-import $ from 'jquery';
+import firebase from 'firebase/app';
 import 'bootstrap';
 import './index.scss';
+import apiKeys from '../db/apiKeys.json';
 
 import createNavbar from './component/Navbar/navbar';
+import loginButton from './component/Auth/auth';
 
-const turkey = () => {
-  $('#turkey').html('yummyy turkey');
-};
-turkey();
+
 const initializeApp = () => {
+  firebase.initializeApp(apiKeys.firebaseKeys);
   createNavbar();
+  loginButton();
 };
 initializeApp();
