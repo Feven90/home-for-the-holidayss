ab0882a47065db9d0dd993e7edb65fbd1de261a6
feven mulugeta <fevenmulugeta@fevens-MacBook-Pro.local>
2018-11-13 20:50:07 -0600

    logout button is done

--- src/component/Navbar/navbar.js
+++ src/component/Navbar/navbar.js
@@ -1,11 +1,52 @@
 import $ from 'jquery';
-
+import firebase from 'firebase/app';
+import 'firebase/auth';
 import 'bootstrap';
 import './navbar.scss';
 
+const navbarEvents = () => {
+  $('.nav-link').on('click', (e) => {
+    console.log(e.target.id);
+    if (e.target.id === 'navbar-button-logout') {
+      firebase.auth().signOut().then(() => {
+        console.log('you logged out');
+      }).catch((err) => {
+        console.error('you still logged in', err);
+      });
+    } else {
+      console.log(e.target.id);
+    }
+  });
+};
 const createNavbar = () => {
-  const domString = 'hi';
+  const domString = `<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <a class="navbar-brand" href="#">Holidays</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav ml-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a id="navbar-button-auth" class="nav-link">Authentication</a>
+      </li>
+      <li class="nav-item">
+          <a id="navbar-button-holidays" class="nav-link">Holidays</a>
+      </li>
+      <li class="nav-item">
+          <a id="navbar-button-friends" class="nav-link">Friends</a>
+      </li>
+      <li class="nav-item">
+          <a id="navbar-button-logout" class="nav-link">Logout</a>
+      </li>
+    </ul>
+  </div>
+</nav>`;
   $('#navbar').html(domString);
+  navbarEvents();
 };
 
 export default createNavbar;
