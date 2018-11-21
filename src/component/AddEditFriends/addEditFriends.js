a173eb51413d6f9fbc00a78c8d3a940530349c82
feven mulugeta <fevenmulugeta@fevens-MacBook-Pro.local>
2018-11-20 19:06:27 -0600

    promise for add

--- src/component/AddEditFriends/addEditFriends.js
+++ src/component/AddEditFriends/addEditFriends.js
@@ -1,7 +1,8 @@
 import $ from 'jquery';
+import authHelpers from '../../helpers/authHelpers';
 
 const formBuilder = () => {
-    const form = `
+  const form = `
     <div class="form-group">
       <label for="form-friend-name">Name: </label>
       <input type="text" class="form-control" id="form-friend-name" placeholder="John Smith">
@@ -24,12 +25,21 @@ const formBuilder = () => {
       <input type="text" class="form-control" id="form-friend-relationship" placeholder="complicated">
     </div>
     <button type="submit" class="btn btn-primary">Submit</button>
-  `
-}
+  `;
+  return form;
+};
 
 const gettingFriendFromForm = () => {
   const friend = {
-      "name": ${form-friend-name},
+    name: $('#form-friend-name').val(),
+    address: $('#form-friend-address').val(),
+    email: $('#form-friend-email').val(),
+    relationship: $('#form-friend-relationship').val(),
+    phoneNumber: $('form-friend-phone').val(),
+    isAvoiding: false,
+    uid: authHelpers.getCurrentUid(),
+  };
+  console.log(friend);
+};
 
-  }
-}
+export default { gettingFriendFromForm, formBuilder };
--- src/component/FriendsPage/friendsPage.js
+++ src/component/FriendsPage/friendsPage.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 import $ from 'jquery';
 import apiKeys from '../../../db/apiKeys.json';
 import authHelpers from '../../helpers/authHelpers';
+import friendsData from '../../helpers/data/friendsData';
 
 const printSingleFriend = (friend) => {
   const friendString = `
@@ -20,10 +21,11 @@ const printSingleFriend = (friend) => {
 const getSingleFriend = (e) => {
   // firebase id
   const friendId = e.target.dataset.dropdownId;
-  axios.get(`${apiKeys.firebaseKeys.databaseURL}/friends/${friendId}.json`)
-    .then((result) => {
-      const singleFriend = result.data;
-      singleFriend.id = friendId;
+  friendsData.getSingleFriend(friendId)
+    .then((singleFriend) => {
+    //   const singleFriend = result.data; we can get rid of this, it is already
+    // done in friendsData promise
+    //   singleFriend.id = friendId;
       printSingleFriend(singleFriend);
     })
     .catch((error) => {
@@ -53,17 +55,17 @@ const buildDropdown = (friendsArray) => {
 
 const friendsPage = () => {
   const uid = authHelpers.getCurrentUid();
-  axios.get(`${apiKeys.firebaseKeys.databaseURL}/friends.json?orderBy="uid"&equalTo="${uid}"`)
-    .then((results) => {
-      const friendsObject = results.data;
-      const friendsArray = [];
-      if (friendsObject != null) {
-        Object.keys(friendsObject).forEach((friendId) => {
-          friendsObject[friendId].id = friendId; // taking friend1, friend2...and putting it into
-          // the object with "id" key
-          friendsArray.push(friendsObject[friendId]);
-        });
-      }
+  friendsData.getAllFriends(uid)
+    .then((friendsArray) => {
+    //   const friendsObject = results.data;
+    //   const friendsArray = [];
+    //   if (friendsObject != null) {
+    //     Object.keys(friendsObject).forEach((friendId) => {
+    //       friendsObject[friendId].id = friendId; // taking friend1, friend2...and putting it into
+    //       // the object with "id" key
+    //       friendsArray.push(friendsObject[friendId]);
+    //     });
+    //   }
       buildDropdown(friendsArray);
     })
     .catch((error) => {
